e7c766d0ac80285b17f1ddeacad52312fab84650
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-02-28 00:05:50 +0530

    try catch $ switch cases

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -19,3 +19,50 @@ public class HotelReservationSystem {
             System.out.println(e.getMessage());
         }
 
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            while(true){
+                System.out.println();
+                System.out.println("HOTEL MANAGEMENT SYSTEM");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("1. Reserve a room");
+                System.out.println("2. View Reservations");
+                System.out.println("3. Get Room Number");
+                System.out.println("4. Update Reservations");
+                System.out.println("5. Delete Reservations");
+                System.out.println("0. Exit");
+                System.out.print("Choose an option: ");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        reserveRoom(connection, scanner);
+                        break;
+                    case 2:
+                        viewReservations(connection);
+                        break;
+                    case 3:
+                        getRoomNumber(connection, scanner);
+                        break;
+                    case 4:
+                        updateReservation(connection, scanner);
+                        break;
+                    case 5:
+                        deleteReservation(connection, scanner);
+                        break;
+                    case 0:
+                        exit();
+                        scanner.close();
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Try again.");
+                }
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
