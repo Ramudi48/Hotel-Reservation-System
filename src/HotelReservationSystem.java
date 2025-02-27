305ae8670fa929774a956d5247b90fbb85a964ee
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-02-28 00:02:36 +0530

    first commit

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,15 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Scanner;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
+
+public class HotelReservationSystem {
+    private static final String url = "jdbc:mysql://localhost:3000/hotel_db";
+    private static final String username = "";
+    private static final String password = "";
+
+
